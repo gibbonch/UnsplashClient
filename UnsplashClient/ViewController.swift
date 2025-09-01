ff29985a0c02331227abeae981b6ed5ae87c0a22
gibbonch <toropov280304@icloud.com>
2025-09-01 19:32:17 +0300

    Initial Commit

---
+++ UnsplashClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashClient
+//
+//  Created by Александр Торопов on 01.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
